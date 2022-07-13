460af3009fc060eaee5651b1a3c21f15dc26f4e3
Jaime Nevado farfan <jnevado-@c2r9s2.42malaga.com>
2022-07-13 15:36:30 +0200

    print integer

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,32 @@
 /*   By: jnevado- <jnevado-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 13:13:09 by jnevado-          #+#    #+#             */
-/*   Updated: 2022/06/27 13:20:05 by jnevado-         ###   ########.fr       */
+/*   Updated: 2022/07/13 13:09:42 by jnevado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
-
-int	ft_printf(char const *input, ...)
+void	ft_write(char c, int *cont)
 {
-	return ;
+	write(1, &c, 1);
+	*cont += 1;
+}
+
+int	ft_printf(char const *str, ...)
+{
+	int		i;
+	int		cont;
+	va_list	args;
+
+	i = 0;
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == "%")
+			ft_all(*str);
+		ft_write(str[i], &cont);
+		i++;
+	}
+	return (cont);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,16 +6,19 @@
 /*   By: jnevado- <jnevado-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 13:40:00 by jnevado-          #+#    #+#             */
-/*   Updated: 2022/06/27 13:43:16 by jnevado-         ###   ########.fr       */
+/*   Updated: 2022/07/13 12:31:20 by jnevado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdio.h"
-#include "unistd.h"
-#include "stdarg.h"
-#include "stdlib.h"
-#include "stddef.h"
+#ifndef FT_PRINTF_H
 
-ft_printf_char(char c);
-ft_prinft_string(char *str);
-ft_printf_pointer(void *ptr);
+# define FT_PRINTF_H
+
+# include "unistd.h"
+# include "stdarg.h"
+
+int	ft_printf_char(char c);
+int	ft_prinft_string(char *str);
+int	ft_printf_pointer(void *ptr);
+
+#endif
